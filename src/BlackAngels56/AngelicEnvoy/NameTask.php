f8f1e0cb69d2e8fbb471a11442cbbc3676c0ef3e
BlackAngels56 <67102721+BlackAngels56@users.noreply.github.com>
2021-04-27 01:08:52 +0300

    Create NameTask.php

---
+++ src/BlackAngels56/AngelicEnvoy/NameTask.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlackAngels56\AngelicEnvoy;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\Server;
+
+class NameTask extends Task {
+    public static $functions = [];
+    
+    public function onRun($tick) {
+        foreach (self::$functions as $f){
+            call_user_func($f);
+        }
+    }
+}
