044a97f5981eac5ceb9ff565f24c25303e8ff004
BlackAngels56 <67102721+BlackAngels56@users.noreply.github.com>
2021-04-27 01:05:33 +0300

    Create CountTask.php

---
+++ src/BlackAngels56/AngelicEnvoy/CountTask.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlackAngels56\AngelicEnvoy;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\Server;
+
+class CountTask extends Task {
+    private $plugin;
+    private $area ;
+    private $sec; 
+    private $cs = 11 ;
+    
+    public function __construct($area,$plugin){
+    $this->plugin = $plugin;
+    $this->area = $area;
+    $this->sec = $plugin->countdown;
+  }
+    public function onRun($tick) {
+        --$this->cs;
+        $s = $this->cs ;
+        if($s <= 10 and $s > 0){
+            $this->plugin->getServer()->broadcastMessage("§6Envoys will be spawned in §3$s §6 seconds in §3" . $this->area);
+        }
+        if($s == 0){
+            $this->plugin->start($this->area);
+        }
+        if($s <= 0){
+            --$this->sec;
+            $secunde = $this->sec;
+            if($secunde <= 0){
+                            $this->plugin->getServer()->broadcastMessage("§4Envoy items will be dropped");
+                           unset($this->plugin->areaStatus[$this->area]);
+                    $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+            }
+        }
+        
+    }
+}
